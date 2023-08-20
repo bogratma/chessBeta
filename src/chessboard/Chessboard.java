21b0432c60e916190db2b07b109981f1b3e61e9b
Vlad <vladik200409@mail.ru>
2023-08-20 22:16:54 +0600

    DisplayBoard

--- src/chessboard/Chessboard.java
+++ src/chessboard/Chessboard.java
@@ -24,6 +24,20 @@ public class Chessboard {
      * Отображение доски (псевдографика или иной способ)
      */
     public void displayBoard(){
-
+        for(int i=0;i<8;i++){
+            System.out.print(i+1);
+            for(int j=0;j<8;j++){
+                if(i<2 || i>5){
+                    System.out.print("\tP");
+                }
+                else {
+                    System.out.print("\t*");
+                }
+            }System.out.println();
+        }
+        System.out.println();
+        for(int i=97;i<105;i++){
+            System.out.print("\t"+(char)i);
+        }
     }
 }
