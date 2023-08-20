8335a3e5563b114f468103fbae23686dad7ff1f7
Vlad <vladik200409@mail.ru>
2023-08-21 00:12:31 +0600

    test DisplayBoard cords

--- src/chessboard/Chessboard.java
+++ src/chessboard/Chessboard.java
@@ -6,7 +6,7 @@ import javax.xml.transform.Source;
 
 public class Chessboard {
     private Piece[][] board;
-    private Pawn pawn = new Pawn();
+
     private King king = new King();
     private Knight knight = new Knight();
     private Bishop bishop = new Bishop();
@@ -34,14 +34,15 @@ public class Chessboard {
      * Отображение доски (псевдографика или иной способ)
      */
     public void displayBoard(){
+        Pawn pawn = new Pawn() ;
         String[] mainPiecesWhite = {rook.getSymbol(), knight.getSymbol(), bishop.getSymbol(), queen.getSymbol(),king.getSymbol(),rook.getSymbol(), knight.getSymbol(), bishop.getSymbol()};
         String[] mainPiecesBlack = {rook.getSymbol(), knight.getSymbol(), bishop.getSymbol(), king.getSymbol(),queen.getSymbol(),rook.getSymbol(), knight.getSymbol(), bishop.getSymbol()};
         for(int i=0;i<8;i++) {
             System.out.print(i + 1);
             for (int j = 0; j < 8; j++) {
-                cords[i][j] = j;
                 if (i == 1 || i == 6) {
                     System.out.print("\t" + pawn.getSymbol());
+                    pawn = new Pawn(i,j);
                 }
                 else if(i==0){
                     System.out.print("\t" + mainPiecesWhite[j]);
@@ -59,6 +60,8 @@ public class Chessboard {
         for(int i=97;i<105;i++){
             System.out.print("\t"+(char)i);
         }
+        for(int i=0;i<8;i++){
+        System.out.println("X is: "+ pawn.getX() +" Y is: "+pawn.getY());
+        }
     }
-
 }
--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,6 +1,23 @@
 package piece;
 
 public class Pawn implements Piece{
+    private int x,y;
+
+    public Pawn(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Pawn() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
     @Override
     public boolean isValidMove(int sourceX, int sourceY, int destinationX, int destinationY) {
